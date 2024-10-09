66217c3047f2b60021cafc936db0bf8e3b30731e
YoussefSheta22 <158490446+YoussefSheta22@users.noreply.github.com>
2024-10-09 19:30:51 -0400

    Update getPlayers.js

--- frontend/research/graphql/getPlayers.js
+++ frontend/research/graphql/getPlayers.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const apiKey = '5dab2604afc042bdb0aa4dddd9f*****';  // Replace with your API key
+const apiKey = '*****';  // Replace with your API key
 const apiUrl = `https://api.sportsdata.io/v3/nfl/scores/json/PlayersByAvailable?key=${apiKey}`;
 
 // Define and export the function to fetch player profiles
